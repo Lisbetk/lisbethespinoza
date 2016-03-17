5e3d033571fe3eec6c8ff5851cde8f9b6179f225
lisbet <lisbetfelix62@gmail.com>
2016-03-17 02:11:08 -0500

    trabajo web

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Aria</title>
+	<!-- latest compiled and minified CSS -->
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">OnniAria-Chan</a> <img src="public/imagenes/25.jpg" alt="50">
+				</div>
+		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="http://www.minitokyo.net/">imagenes</a></li>
+					</ul>
+
+					<nav class="navbar-form navbar-left">		
+						<div id="search-box" class="main-search-box"> <form method="GET" action="http://www.animeyt.tv/busqueda" accept-charset="UTF-8"><input placeholder="Buscar animes" name="terminos" type="text">
+						</div>						
+					
+					</nav>
+					</form>
+					<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#">Pelicula</a></li>
+					<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">tipos<b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a a href="http://jkanime.net/genero/accion/" target="http://jkanime.net/genero/accion/" >Accion</a></li>
+								<li><a a href="http://jkanime.net/genero/artes-marciales/" target="http://jkanime.net/genero/artes-marciales/">Arte marciales</a></li>
+								<li><a href="http://jkanime.net/genero/autos/" target="http://jkanime.net/genero/autos/">Auto</a></li>
+								<li><a href="http://jkanime.net/genero/aventura/" target="http://jkanime.net/genero/aventura/">aventuras</a></li>
+								<li><a a href="http://jkanime.net/genero/colegial/" target="http://jkanime.net/genero/colegial/">colegial</a></li>
+								<li><a a href="http://jkanime.net/genero/comedia/" target="http://jkanime.net/genero/comedia/">comedia</a></li>
+								<li><a a href="http://jkanime.net/genero/harem/" target="http://jkanime.net/genero/harem/">Harem</a></li>
+								<li><a a href="http://jkanime.net/genero/policial/" target="http://jkanime.net/genero/policial/">Policial</a></li>
+								<li><a a href="http://jkanime.net/genero/parodia/" target="http://jkanime.net/genero/parodia/"> parodia</a></li>
+								<li><a a href="http://jkanime.net/genero/terror/" target="http://jkanime.net/genero/terror/">Terror</a></li>
+								<li><a a href="http://jkanime.net/genero/vampiros/" target="http://jkanime.net/genero/vampiros/">Vanpiros</a></li>
+								<li><a a href="http://jkanime.net/genero/misterio/" target="http://jkanime.net/genero/misterio/">Misterio</a></li>
+								<li><a a href="http://jkanime.net/genero/militar/" target="http://jkanime.net/genero/militar/">Militar</a></li>
+								<li><a a href="http://jkanime.net/genero/deportes/" target="http://jkanime.net/genero/deportes/">Deporte</a></li>
+								<li><a a href="http://jkanime.net/genero/magia/" target="http://jkanime.net/genero/magia/">magia</a></li>
+								<li><a a href="http://jkanime.net/genero/juegos/" target="http://jkanime.net/genero/juegos/">juegos</a></li>
+							</ul>
+						</li>
+						<li class="active"><a href="http://listas.20minutos.es/lista/los-mejores-openings-del-anime-298176/"> opening</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">ir al final y clic en el(ooooo)  <b class=""></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Romance</a></li>
+								<li><a href="#">Accion</a></li>
+								<li><a href="#">Terror</a></li>
+								<li><a href="#">Aventura</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
+		<!-- carousel -->
+<div id="carousel-id" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+		<li data-target="#carousel-id" data-slide-to="3" class="active"></li>
+		<li data-target="#carousel-id" data-slide-to="4" class="active"></li>
+		<li data-target="#carousel-id" data-slide-to="5" class="active"></li>
+		<li data-target="#carousel-id" data-slide-to="6" class="active"></li>
+	
+		<li data-target="#carousel-id" data-slide-to="8" class="active"></li>
+		<li data-target="#carousel-id" data-slide-to="9" class="active"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="item">
+			<img data-src="public/imagenes/1.jpg" alt="First slide" src="public/imagenes/1.jpg"> 
+		</div>
+		<div class="item">
+			<img data-src="public/imagenes/2.jpg" alt="Second slide" src="public/imagenes/2.jpg">
+			
+		</div>
+		<div class="item active">
+			<img data-src="public/imagenes/3.jpg" alt="Third slide" src="public/imagenes/3.jpg">
+			
+		</div>	
+		<div class="item active">
+			<img data-src="public/imagenes/4.jpg" alt="Third slide" src="public/imagenes/4.jpg">
+			
+		</div>			
+		<div class="item">
+			<img data-src="public/imagenes/5.jpg" alt="First slide" src="public/imagenes/5.jpg"> 
+		</div>
+		<div class="item">
+		
+			<img data-src="public/imagenes/6.jpg" alt="Second slide" src="public/imagenes/6.jpg">
+			
+		</div>
+		<div class="item active">
+
+			<img data-src="public/imagenes/8.jpg" alt="Third slide" src="public/imagenes/8.jpg">
+			
+		</div>
+		<div class="item">
+
+			<img data-src="public/imagenes/9.jpg" alt="First slide" src="public/imagenes/9.jpg"> 
+		</div>
+
+		<div class="item">
+			<img data-src="public/imagenes/10.jpg" alt="Second slide" src="public/imagenes/10.jpg">
+			
+		</div>						
+		
+		</div>
+	</div>
+	<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+
+	</div>
+
+<script src="public/js/jquery-1.12.1.js"></script>
+<script src="public/js/bootstrap.js"></script>
+
+<video autobuffer autoloop loop controls width="2" autoplay="public/video/Because I'm stupid (Boys over flowers) (español).mp4">
+	<source src="public/video/Because I'm stupid (Boys over flowers) (español).mp4">
+	
+</video>
+	
+</body>
+</html>
